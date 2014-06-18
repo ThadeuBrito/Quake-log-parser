4a05a438debd9bdaec87a734805875f21f48616f
Thadeu Brito <itvbrito@gmail.com>
2014-06-18 09:40:49 -0300

    Editing a player when 'ClientUserinfoChanged:'

--- parser_log.rb
+++ parser_log.rb
@@ -10,6 +10,7 @@ class ParserLog
     @log.each_line do |line|
       self.start_game(@game_id) if line.to_s.split(' ')[1] == 'InitGame:'
       self.new_player((line.to_s.split(' ')[2]).to_i) if line.to_s.split(' ')[1] == 'ClientConnect:'
+      self.edit_player( (line.to_s.split(' ')[2]).to_i, (line.to_s.split(' ')[3]) ) if line.to_s.split(' ')[1] == 'ClientUserinfoChanged:'
     end
   end
 
@@ -23,6 +24,12 @@ class ParserLog
 
   def new_player(player_id)
     @players << {"id" => player_id, "name" => "", "kills" => 0} unless @players.map{|i| i.to_a[0][1]}.include?(player_id )
+  end
+
+  def edit_player(player_id, line)
+    for player in @players
+      player["name"] = line.split('n\\').last.split('\\t').first if player["id"] == player_id
+    end
   end
 
 end
