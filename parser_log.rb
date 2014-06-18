6044226899a5b0398ac828a67e11cdae78d43870
Thadeu Brito <itvbrito@gmail.com>
2014-06-18 09:37:41 -0300

    Creating a new player

--- parser_log.rb
+++ parser_log.rb
@@ -9,6 +9,7 @@ class ParserLog
   def analyze
     @log.each_line do |line|
       self.start_game(@game_id) if line.to_s.split(' ')[1] == 'InitGame:'
+      self.new_player((line.to_s.split(' ')[2]).to_i) if line.to_s.split(' ')[1] == 'ClientConnect:'
     end
   end
 
@@ -18,6 +19,10 @@ class ParserLog
     @game_id    +=    1
     @players     =    []
     @game        =    {"game_#{@game_id}" => { "total_kills" => "", "players" => [], "kills" => {} } }
+  end
+
+  def new_player(player_id)
+    @players << {"id" => player_id, "name" => "", "kills" => 0} unless @players.map{|i| i.to_a[0][1]}.include?(player_id )
   end
 
 end
