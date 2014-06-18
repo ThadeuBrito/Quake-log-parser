859a1ae8be26cff99771db52803890d54e2af68d
Thadeu Brito <itvbrito@gmail.com>
2014-06-18 09:44:00 -0300

    Count kills and assigning to players

--- parser_log.rb
+++ parser_log.rb
@@ -11,6 +11,7 @@ class ParserLog
       self.start_game(@game_id) if line.to_s.split(' ')[1] == 'InitGame:'
       self.new_player((line.to_s.split(' ')[2]).to_i) if line.to_s.split(' ')[1] == 'ClientConnect:'
       self.edit_player( (line.to_s.split(' ')[2]).to_i, (line.to_s.split(' ')[3]) ) if line.to_s.split(' ')[1] == 'ClientUserinfoChanged:'
+      self.new_kill(line.to_s.split(' ')[2].to_i, line.to_s.split(' ')[3].to_i, @game_id) if line.to_s.split(' ')[1] == 'Kill:'
     end
   end
 
@@ -29,6 +30,19 @@ class ParserLog
   def edit_player(player_id, line)
     for player in @players
       player["name"] = line.split('n\\').last.split('\\t').first if player["id"] == player_id
+    end
+  end
+
+  def new_kill(killer_id, dead_id, game_id)
+    @game["game_#{game_id}"]["total_kills"] = (@total_kills += 1)
+    if killer_id == 1022
+      for player in @players
+        player["kills"] -= 1 if player["id"] == dead_id
+      end
+    else
+      for player in @players
+        player["kills"] += 1 if player["id"] == killer_id
+      end
     end
   end
 
