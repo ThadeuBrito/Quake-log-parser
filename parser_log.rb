aa1a2d2922393f989ea8b5370be8decd517a86bc
Thadeu Brito <itvbrito@gmail.com>
2014-06-18 09:34:28 -0300

    Method to start game

--- parser_log.rb
+++ parser_log.rb
@@ -7,6 +7,17 @@ class ParserLog
   end
 
   def analyze
+    @log.each_line do |line|
+      self.start_game(@game_id) if line.to_s.split(' ')[1] == 'InitGame:'
+    end
+  end
+
+  def start_game(game_id)
+    @total_kills =    0
+    @kills       =    {}
+    @game_id    +=    1
+    @players     =    []
+    @game        =    {"game_#{@game_id}" => { "total_kills" => "", "players" => [], "kills" => {} } }
   end
 
 end
